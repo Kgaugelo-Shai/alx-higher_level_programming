5ae5054bb9d06ed95dbf8e500c097af4666ddd5f
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-02-13 02:46:03 +0200

    test 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,6 +29,47 @@ class Square(Rectangle):
         self.width = value
         self.width = value
 
+    def update(self, *args, **kwargs):
+        """Updates the dimensions of the square object.
+
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument should be id attribute
+                - 2nd argument should be  size attribute
+                - 3rd argument should be x attribute
+                - 4th argument should be y attribute
+            **kwargs (dict): New key/value pairs of attribute to the instance.
+        """
+        if args and len(args) != 0:
+            c = 0
+            for a in args:
+                if c == 0:
+                    if c is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = a
+                elif c == 1:
+                    self.size = a
+                elif c == 2:
+                    self.x = a
+                elif c == 3:
+                    self.y = a
+                c += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+
     def __str__(self):
         """Returns a string representation of a Square."""
         string = "[Square] ({}) {}/{} - {}"

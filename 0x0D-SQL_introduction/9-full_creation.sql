49650713417380e094b044b354547cff90fb075b
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-03-13 05:11:27 +0200

    task 9fix

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Script that creates a table second_table in the database
-CREATE IF NOT EXISTS second_table (
+CREATE TABLE IF NOT EXISTS second_table (
        id INT,
        name VARCHAR(256),
        score INT

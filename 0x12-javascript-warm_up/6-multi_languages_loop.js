bb1d3a81edd17fe1881ddcebb936e93326018f35
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-04-11 02:22:20 +0200

    task 6 fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (const node of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+for (const node of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
   console.log(node);
 }

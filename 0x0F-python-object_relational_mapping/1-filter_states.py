9f14019f7821d0ae03c9ae22bd0aeb1cd306a5d5
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-04-15 01:25:17 +0200

    task 1 fix 3

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     # create a cursor for the connection
     cursor = conn_db.cursor()
     # execute query with cursor
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'"
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'"
                    " ORDER BY states.id")
     # Fetch the results
     rows = cursor.fetchall()

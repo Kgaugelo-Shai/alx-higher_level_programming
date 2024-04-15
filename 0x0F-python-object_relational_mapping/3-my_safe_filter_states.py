aae8dcc2b87d4a754b8f04124e9d609b1dbf2507
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-04-15 02:02:21 +0200

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" script that takes in arguments and displays all values in the states table
+    of hbtn_0e_0_usa where name matches the argument
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    conn_db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                              passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    # create a cursor
+    cursor = conn_db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s", (sys.argv[4],))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    conn_db.close()

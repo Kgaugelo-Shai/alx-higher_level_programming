971fda850d0260ba0aa841d1941829eb5d2107ac
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-04-27 00:53:31 +0200

    task 0 and readme

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Python script that fetches https://alx-intranet.hbtn.io/status """
+
+from urllib import (request)
+
+
+if __name__ == "__main__":
+    web_reqst = request.Request("http://alx-intranet.hbtn.io/status")
+    with request.urlopen(web_reqst) as response:
+        body = response.read()
+
+        print("Body response")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf8")))

c72f35030da3cb2a76a617f18bcd9e733e8713a9
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-04-27 02:37:45 +0200

    task 100 advanced

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""  list 10 commits (from the most recent to oldest) of the repository “rails”
+     by the user “rails. You must use the GitHub API, here is the documentation
+     https://developer.github.com/v3/repos/commits/
+     Print all commits by: `<sha>: <author name>` (one by line)
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/repos/{}/{}'.format(sys.argv[1], sys.argv[2])
+    url += '/commits?per_page=10'
+    reqst = requests.get(url)
+    commits = reqst.json()
+    for c in commits:
+        print("{}: {}".format(c.get("sha"),
+                              c.get("commit").get("author").get("name")))

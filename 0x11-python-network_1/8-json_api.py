d9c015b353c0ff7ecf0cd6e7ba48e12ab237b318
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-04-27 02:14:27 +0200

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Script in a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) == 2:
+        reqst = requests.post(url, data={'q': argv[1]})
+    else:
+        reqst = requests.post(url, data={'q': ""})
+    try:
+         response = reqst.json()
+        if response:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")

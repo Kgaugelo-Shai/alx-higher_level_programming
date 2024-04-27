0d24fa2098c4bb4025fc3fdad3fe0d9ae5153eea
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-04-27 02:27:51 +0200

    task 10 fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,11 +4,9 @@
 """
 import sys
 import requests
-from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     reqst = requests.get("https://api.github.com/user", auth=(sys.argv[1],
                                                               sys.argv[2]))
     print(reqst.json().get("id"))

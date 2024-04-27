f5ec5d25196152eec77fed41a70d9ac55473122c
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-04-27 03:00:47 +0200

    task 100 fix 9

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,7 +10,7 @@ import sys
 
 if __name__ == "__main__":
     url = 'https://api.github.com/repos/{}/{}/commits/'.format(sys.argv[1],
-                                                              sys.argv[2])
+                                                               sys.argv[2])
 
     reqst = requests.get(url)
     commit_js = reqst.json()

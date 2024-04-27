5fac9349ad2063aa1422bc3112f5a6b8e09abc35
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-04-27 02:06:57 +0200

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Script that takes in a URL, sends a request to the URL and displays the
+    body of the response.
+"""
+
+import requests
+from sys
+
+if __name__ == "__main__":
+    reqst = requests.get(sys.argv[1])
+    if int(reqst.status_code) < 400:
+        print(reqst.text)
+    else:
+        print("Error code: {}".format(reqst.status_code))

a734828c4ca041918d8096f1590d20f2b43379d9
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-04-27 03:09:31 +0200

    task 0 fix 3

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     print("Body response:")
     print("\t- type: {}".format(type(cont)))
     print("\t- content: {}".format(cont))
-    print("\t- utf8 content: {}".format(cont.decode("utf-8")))
+    print("\t- utf8 content: {}".format(cont.decode("utf8")))

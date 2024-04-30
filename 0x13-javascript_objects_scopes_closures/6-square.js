4d3495ff8908e6c857a7d2f39ae81ca10c6406f2
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-04-30 12:44:20 +0200

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const SquareBase = require('./5-square');
+
+class Square extends SquareBase {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let sqr = '';
+      for (let j = 0; j < this.width; j++) {
+        sqr += c;
+      }
+	console.log(sqr);
+    }
+  }
+}
+
+module.exports = Square;

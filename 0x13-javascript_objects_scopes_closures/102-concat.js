8a981cf50903520827c71c020c0e81cb10ae03bb
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-04-30 15:53:16 +0200

    task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,13 +1,21 @@
 #!/usr/bin/node
-const { readFile, writeFile } = require('fs');
-const { argv } = require('process');
+const fs = require('fs');
+const f1 = process.argv[2];
+const f2 = process.argv[3];
+const f3 = process.argv[4];
 
-const contents = (file) => {
-  return readFile(file, 'utf8');
-};
+function readfiles (file) {
+  return fs.readFileSync(file);
+}
 
-const concatStr = contents(argv[2]) + '' + contents(argv[3]);
-
-writeFile(argv[4], concatStr, 'utf8', err => {
-  if (err) throw err;
-});
+function writefiles (file, content) {
+  fs.writeFile(file, content, (err) => {
+    if (err) {
+      return console.error(err);
+    }
+  });
+}
+let newFile = '';
+newFile = readfiles(f1);
+dnewFile += readfiles(f2);
+writefiles(f3, newFile);

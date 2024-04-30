b8ec5a9bcaed7afd0ff80b326fb1b5672a3cd1f9
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-04-30 16:33:09 +0200

    task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Adds Louisiana to list of state names
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state_name = State(name='Louisiana')
+    session.add(new_state)
+    new_inst = session.query(State).filter_by(name='Louisiana').first()
+    print(new_inst.id)
+    session.commit()

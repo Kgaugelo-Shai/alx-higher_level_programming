3f46bf17e8f556f9b67eb8e04a3a14a5c8cb6957
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-05-23 04:33:26 +0200

    semistandard fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,5 +5,5 @@ const URL = 'https://swapi-api.hbtn.io/api/films/';
 const episode = process.argv[2];
 
 request(URL + episode, function (error, response, body) {
-    console.log(error || JSON.parse(body).title);
+  console.log(error || JSON.parse(body).title);
 });

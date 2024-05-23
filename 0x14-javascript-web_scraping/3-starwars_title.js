b46b8326feadc442d4b9507e3acab12cb7dc307c
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-05-23 04:32:44 +0200

    3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const request = require('request');
+const URL = 'https://swapi-api.hbtn.io/api/films/';
+const episode = process.argv[2];
+
+request(URL + episode, function (error, response, body) {
+    console.log(error || JSON.parse(body).title);
+});
